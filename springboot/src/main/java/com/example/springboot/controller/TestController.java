dd044daebd4d601c4fad58f29a80f194ac55cbd0
fengqi <fqUYd55e>
2023-01-16 17:26:03 +0800

    #springboot init

--- springboot/src/main/java/com/example/springboot/controller/TestController.java
+++ springboot/src/main/java/com/example/springboot/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.example.springboot.controller;
 
+// 注意: commons-logging 通过 LogFactory.getLog() 获取日志，而 log4j 通过 Logger.getLog() 获取
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,11 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    private Log logger = LogFactory.getLog(TestController.class.getName());
+
     @RequestMapping(value = "/api/testApi", method = RequestMethod.GET)
     public String testApi() {
+        logger.info("hello world");
         return "hello world";
     }
 
@@ -20,6 +26,7 @@ public class TestController {
         Map<String, String> res = new HashMap<>();
         res.put("code", "200");
         res.put("msg", "hello world");
+        logger.info(res);
         return res;
     }
 
